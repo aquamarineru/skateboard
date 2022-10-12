a603a843b6e8ab41e19c0eb1fadb442fd042559f
Victorfernandezsaez <v.ferna.saez@gmail.com>
2022-10-12 15:57:07 +0100

    displayedData

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import useContentful from './useContentful'
+import Products from './Products'
 
 function App() {
   const [ products, setProducts ] = useState([])
   const { getProducts } = useContentful()
 
   useEffect(() => {
-    getProducts().then((response) => console.log(response))
-  })
-  return (
-    <div className="App">
-    
-    </div>
-  )
+    getProducts().then((response) => setProducts(response))
+  });
+  return (<div>{products.map((product, index) => product.brand)}</div>)
 }
 
 export default App
---
+++ src/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Products = ({ products }) => {
+    return('hello' )
+    
+}
+
+export default  Products
