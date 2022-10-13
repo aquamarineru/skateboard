783232d570a52f313def6750d206fa01ac7727ef
Malaiz <malaizdaud@gmail.com>
2022-10-13 09:22:00 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import useContentful from './useContentful'
+import ProductCard from './ProductCard';
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { getProducts } = useContentful();
+  const [products, setProducts] = useState ([])
+  
 
-  return (
-    <div className="App">
+  useEffect(() => {
+    getProducts().then((response) => setProducts(response));
+  });
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Victor's Skateboards</h1>
+           {products.map((product, index) => <ProductCard key={index} product={product} />)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    );  
+
 }
 
+
 export default App
+
---
+++ src/useContentful.jsx
@@ -0,0 +1,43 @@
+import { createClient } from "contentful"
+
+const useContentful = () => {
+
+   const client = createClient({
+        space: "2qxrnxo2qe5m",
+        accessToken: "ATKEz-nthZMy07n2dV5HBW1yq-n9LKnxn8Ku7tG0nuQ",
+        host: "https://cdn.contentful.com"
+    });
+
+    const getProducts = async () => {
+        try {
+            const entries = await client.getEntries({
+                content_type: "product",
+                select: "fields"
+            })
+
+            const sanitizedEntries = entries.items.map((item) => {
+                const avatar = item.avatar;
+
+                return {
+                    ...item.fields,
+                    avatar
+                }
+           })          
+
+           return sanitizedEntries;
+
+           
+             
+        }
+
+        
+        
+        catch (error) {
+        console.log(`Error fetching data: ${error}`)
+        }
+    };
+    
+    return { getProducts }
+};
+
+export default useContentful
